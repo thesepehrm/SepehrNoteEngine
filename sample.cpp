08ad5af5912f6fffbbde118255f55e4e40ded5d1
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2017-03-17 16:51:58 +0330

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,94 @@
+#include <Windows.h>
+#include "SepehrNoteEngine.h"
+void jingleBells();
+void xmasSong();
+SepehrNoteEngine song(150);
+int main()
+{
+	printf("Playing a loop of \"We wish you a merry christmass\"");
+	while (true)
+	{
+		//jingleBells();
+		xmasSong();
+	}
+	
+
+}
+
+void jingleBells()
+{
+	Beep(song.Note("G", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 2.0));
+
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 2.0));
+
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("G", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("C", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("E", 4), song.NoteValue(1));
+
+	Beep(song.Note("F", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("F", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("F", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("F", 4), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("F", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("D", 4), song.NoteValue(1 / 2.0));
+	Beep(song.Note("G", 4), song.NoteValue(1 / 2.0));
+
+}
+void xmasSong()
+{
+	Beep(song.Note("G", 3), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("C", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("C", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("C", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("B", 3), song.NoteValue(1 / 8.0));
+
+	Beep(song.Note("A", 3), song.NoteValue(1 / 4.0));
+	Beep(song.Note("A", 3), song.NoteValue(1 / 4.0));
+	Beep(song.Note("A", 3), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("D", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("C", 4), song.NoteValue(1 / 8.0));
+
+	Beep(song.Note("B", 3), song.NoteValue(1 / 4.0));
+	Beep(song.Note("G", 3), song.NoteValue(1 / 4.0));
+	Beep(song.Note("G", 3), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("E", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("F", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("E", 4), song.NoteValue(1 / 8.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 8.0));
+
+	Beep(song.Note("C", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("A", 3), song.NoteValue(1 / 4.0));
+	Beep(song.Note("G", 3), song.NoteValue(1 / 8.0));
+	Beep(song.Note("G", 3), song.NoteValue(1 / 8.0));
+
+	Beep(song.Note("A", 3), song.NoteValue(1 / 4.0));
+	Beep(song.Note("D", 4), song.NoteValue(1 / 4.0));
+	Beep(song.Note("B", 3), song.NoteValue(1 / 4.0));
+
+	Beep(song.Note("C", 4), song.NoteValue(1 / 2.0));
+}
