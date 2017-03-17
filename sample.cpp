bd5aac255830581514b3c5df054a468045ea694b
Sepehr Mohammadi <thesepehrm@users.noreply.github.com>
2017-03-17 20:01:32 +0330

    Added Pink Panther :D

--- sample.cpp
+++ sample.cpp
@@ -1,16 +1,21 @@
 #include <Windows.h>
-#include "SepehrNoteEngine.h"
+#include "SepehrNotes.h"
+
 void jingleBells();
 void xmasSong();
+void pinkPanther();
+
 SepehrNoteEngine song(150);
 int main()
 {
-	printf("Sepehr Mohammadi presents\n");
-	printf("Playing a loop of \"We wish you a merry christmas\"");
+	printf("Sepehr Mohammadi Presents...\n");
+	printf("Playing a loop of \"Pink Panther\"");
 	while (true)
 	{
 		//jingleBells();
-		xmasSong();
+		//xmasSong();
+		pinkPanther();
+		
 	}
 	
 
@@ -92,4 +97,37 @@ void xmasSong()
 	Beep(song.Note("B", 3), song.NoteValue(1 / 4.0));
 
 	Beep(song.Note("C", 4), song.NoteValue(1 / 2.0));
+}
+void pinkPanther()
+{
+	for (int octave = 4; octave < 6; octave++)
+	{
+		Beep(song.Note("D#", octave), song.NoteValue(1 / 16.0));
+		Beep(song.Note("E",octave), song.NoteValue(1 / 4.0, 0));
+		Sleep(song.NoteValue(1 / 8.0, 1));
+		Beep(song.Note("F#", octave), song.NoteValue(1 / 16.0));
+		Beep(song.Note("G", octave), song.NoteValue(1 / 4.0, 0));
+		Sleep(song.NoteValue(1 / 8.0, 1));
+		Beep(song.Note("D#", octave), song.NoteValue(1 / 16.0));
+
+		Beep(song.Note("E", octave), song.NoteValue(1 / 8.0, 1));
+		Beep(song.Note("F#", octave), song.NoteValue(1 / 16.0));
+		Beep(song.Note("G",octave), song.NoteValue(1 / 8.0, 1));
+		Beep(song.Note("C", octave+1), song.NoteValue(1 / 16.0));
+		Beep(song.Note("B", octave), song.NoteValue(1 / 8.0, 1));
+		Beep(song.Note("E", octave), song.NoteValue(1 / 16.0));
+		Beep(song.Note("G", octave), song.NoteValue(1 / 8.0, 1));
+		Beep(song.Note("B", octave), song.NoteValue(1 / 16.0));
+
+		Beep(song.Note("Bb", octave), song.NoteValue(1 / 2.0 + 1 / 12.0));
+		Beep(song.Note("A", octave), song.NoteValue(1 / 12.0));
+		Beep(song.Note("G", octave), song.NoteValue(1 / 12.0));
+		Beep(song.Note("E", octave), song.NoteValue(1 / 12.0));
+		Beep(song.Note("D", octave), song.NoteValue(1 / 12.0));
+		Beep(song.Note("E", octave), song.NoteValue(1 / 12.0 + 1 / 2.0));
+
+		Sleep(song.NoteValue(1 / 4.0));
+		Sleep(song.NoteValue(1 / 8.0, 1));
+	}
+	
 }
